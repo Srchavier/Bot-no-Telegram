b1409add944b725866189e6b7f8015922376c63a
Srchavier <eduardug.tinho@hotmail.com>
2016-11-18 16:35:13 -0200

    versao 1.8 com BD.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'script.php';
+
+        ?>
+
+        <form method ="GET" action="script.php">
+            <input type="text" name="mensagem"/>
+            <input type="submit" value="OK" />
+        </form>
+    </body>
+</html>
+
+<!-- nome:Eduardo Rodrigues Fernandes
+     matricula:201611386-->
