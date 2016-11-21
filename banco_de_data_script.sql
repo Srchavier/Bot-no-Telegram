b7164ba237d618599d5ebe5b103a77818a2eb9bf
Srchavier <eduardug.tinho@hotmail.com>
2016-11-20 23:46:04 -0200

    versao 2.1 mais uma funcao.

--- banco_de_data_script.sql
+++ banco_de_data_script.sql
@@ -2,15 +2,14 @@ create database BD_telegram;
 
 use BD_telegram;
 
+select * from BD_resposta;
+
 CREATE TABLE BD_resposta (
     id BIGINT,
     nome VARCHAR(50),
-    mensagem VARCHAR(400),
+    mens_rec VARCHAR(250),
+    num_mega VARCHAR(30),
+    num_lotf varchar(25),
     Data DATETIME NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
 );
-
-
-select * from BD_resposta;
-
-drop table BD_resposta;
