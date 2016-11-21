34203e052fccf14a77b33f47ba26a22631974f07
Srchavier <eduardug.tinho@hotmail.com>
2016-11-20 23:25:06 -0200

    versao 2.1 mais uma funcao.

--- script.php
+++ script.php
@@ -13,16 +13,18 @@ require './BD_txt_Mysql.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-//--------------------------------------------abrir arq id--------------------------------
+
 $file = 'updateId.txt';
 $str = file_get_contents($file);
 $arrUpdateId = explode(',', $str);
-//---------------------------------------------------receber mensagem----------------------
+
 $resultado = recebendo_mensagem::upload();
-//----------------------------------------array mens------------------------
+
+
 $var = count($resultado['result']) - 1;
-//--------------------------------pegando mens-----------------
+
 for ($x = $var; $x > 0; $x--) {
+
     $data = $resultado ['result'][$x]['message']['date'];
     echo "<br>";
     $dataTratada = Data::tratarData($data);
@@ -36,11 +38,13 @@ for ($x = $var; $x > 0; $x--) {
     echo "<br>";
     $updateId = $resultado ['result'][$x]['update_id']; //salva os id no arquivo text
     echo "<br>";
+
+
 //------------------------------------BD--------------------------------------
     $var = null;//inicia null para bd sem sort
-//----------------------------------------------------------------------------
-//
-//-----------------------------------------pegando comandos-------------------
+    $lot= null;
+//---------------------------------------------------------------------------------------------------------
+
     if ($texto == '/start') {
         $texto1 = preg_match('/^.*\/start$/', $texto);
         if (!in_array($updateId, $arrUpdateId)) {
@@ -55,27 +59,44 @@ for ($x = $var; $x > 0; $x--) {
             if ($texto1 == 1) {
                 print ("Seus Numeros da mega : ");
                 echo "<br>";
-                $sort = sorteador_numero::sorteador(); //ativa a funçao sorteador
+                $sort = sorteador_numero::sort_meg_sen(); //ativa a funçao sorteador
                 mensagem::sendMessage($id, $sort); //ativa a funcao mensagem.
                 echo $sort; //tras o resultando.
                 $var = $sort;
                 echo "<br>";
             }
         }
-    } 
+    } else if ($texto == '/Lotofacil') {
+        $texto1 = preg_match('/^.*\/Lotofacil$/', $texto);
+        if (!in_array($updateId, $arrUpdateId)) {
+            if ($texto1 == 1) {
+                print ("Seus Numeros da Lotofacil : ");
+                echo "<br>";
+               $men_lot_facil = sorteador_numero::sort_lot_facil(); //ativa a funçao sorteador
+                mensagem::sendMessage($id, $men_lot_facil); //ativa a funcao mensagem.
+                echo $men_lot_facil; //tras o resultando.
+                $lot = $men_lot_facil;
+                echo "<br>";
+               
+            }
+        }
+    }
+    if (!in_array($updateId, $arrUpdateId)) {
+        $conteudo = "$updateId,$nome,$texto,$var \r\n";
+        BD_txt_Mysql::BD_txt($conteudo, $updateId);
+        file_put_contents($file, $updateId . ',', FILE_APPEND | LOCK_EX);
+    }
+
     //-----------------------------------BD Mysql GUARDANDO OS LOGS--------------------------------------------
-    $ins = "insert into" . " BD_resposta(id,nome,mens_rec,num_mega)" . " VALUES(?, ?, ?, ?)";
+    $ins = "insert into" . " BD_resposta(id,nome,mens_rec,num_mega,num_lotf)" . " VALUES(?, ?, ?, ?, ?)";
     $BD = BD_txt_Mysql::connect()->prepare($ins);
     $BD->bindParam(1, $updateId);
     $BD->bindParam(2, $nome);
     $BD->bindParam(3, $texto);
     $BD->bindParam(4, $var);
+    $BD->bindParam(5, $lot);
     $BD->execute();
-    //------------------------------------BD2 Txt caso o primeiro der erro--------------------------------------
-    if (!in_array($updateId, $arrUpdateId)) {
-        $conteudo = "$updateId,$nome,$texto,$var \r\n";
-        BD_txt_Mysql::BD_txt($conteudo, $updateId);
-        file_put_contents($file, $updateId . ',', FILE_APPEND | LOCK_EX);//salva os id para nao repetir mensagem no bd
-    }
+    //------------------------------------BD Txt caso o primeiro der erro--------------------------------------
+    
 }
 ?>
--- sorteador_numero.php
+++ sorteador_numero.php
@@ -14,7 +14,8 @@
 class sorteador_numero {
     //put your code here
     
-    public static function sorteador(){
+    public static function sort_meg_sen(){
+    
          for ($w = 1; $w <= 6; $w++) {
                     $n[$w - 1] = str_pad(rand(1, 60), 2, '0', STR_PAD_LEFT);
                 }
@@ -23,5 +24,19 @@ class sorteador_numero {
                 
                 return $resultadomegasena;
                 
+                
     }
+      public static function sort_lot_facil(){
+    
+         for ($w = 1; $w <= 5; $w++) {
+                    $n[$w - 1] = str_pad(rand(1, 25), 2, '0', STR_PAD_LEFT);
+                }
+                sort($n);
+                $resultlot = implode(' - ', $n);
+                
+                return $resultlot;
+                
+                
+    }
+    
 }
