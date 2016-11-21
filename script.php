e12d456cb99a2f007b94ec6e10ab23592e58f99c
Srchavier <eduardug.tinho@hotmail.com>
2016-11-20 22:40:50 -0200

    versao 2.0 upload no bd.

--- script.php
+++ script.php
@@ -13,18 +13,16 @@ require './BD_txt_Mysql.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+//--------------------------------------------abrir arq id--------------------------------
 $file = 'updateId.txt';
 $str = file_get_contents($file);
 $arrUpdateId = explode(',', $str);
-
+//---------------------------------------------------receber mensagem----------------------
 $resultado = recebendo_mensagem::upload();
-
-
+//----------------------------------------array mens------------------------
 $var = count($resultado['result']) - 1;
-
+//--------------------------------pegando mens-----------------
 for ($x = $var; $x > 0; $x--) {
-
     $data = $resultado ['result'][$x]['message']['date'];
     echo "<br>";
     $dataTratada = Data::tratarData($data);
@@ -38,19 +36,17 @@ for ($x = $var; $x > 0; $x--) {
     echo "<br>";
     $updateId = $resultado ['result'][$x]['update_id']; //salva os id no arquivo text
     echo "<br>";
-
-
 //------------------------------------BD--------------------------------------
     $var = null;//inicia null para bd sem sort
-//---------------------------------------------------------------------------------------------------------
-
+//----------------------------------------------------------------------------
+//
+//-----------------------------------------pegando comandos-------------------
     if ($texto == '/start') {
         $texto1 = preg_match('/^.*\/start$/', $texto);
         if (!in_array($updateId, $arrUpdateId)) {
             if ($texto1 == 1) {
                 $mesangem = 'Parabens ' . $nome . ' acaba de ser regrista!!!!';
                 mensagem::sendMessage($id, $mesangem); //ativa a funcao mensagem.
-                file_put_contents($file, $updateId . ',', FILE_APPEND | LOCK_EX);
             }
         }
     } else if ($texto == '/megasena') {
@@ -59,7 +55,6 @@ for ($x = $var; $x > 0; $x--) {
             if ($texto1 == 1) {
                 print ("Seus Numeros da mega : ");
                 echo "<br>";
-
                 $sort = sorteador_numero::sorteador(); //ativa a funçao sorteador
                 mensagem::sendMessage($id, $sort); //ativa a funcao mensagem.
                 echo $sort; //tras o resultando.
@@ -68,7 +63,6 @@ for ($x = $var; $x > 0; $x--) {
             }
         }
     } 
-
     //-----------------------------------BD Mysql GUARDANDO OS LOGS--------------------------------------------
     $ins = "insert into" . " BD_resposta(id,nome,mens_rec,num_mega)" . " VALUES(?, ?, ?, ?)";
     $BD = BD_txt_Mysql::connect()->prepare($ins);
@@ -77,11 +71,11 @@ for ($x = $var; $x > 0; $x--) {
     $BD->bindParam(3, $texto);
     $BD->bindParam(4, $var);
     $BD->execute();
-    //------------------------------------BD Txt caso o primeiro der erro--------------------------------------
+    //------------------------------------BD2 Txt caso o primeiro der erro--------------------------------------
     if (!in_array($updateId, $arrUpdateId)) {
-        $conteudo = "$updateId,$nome,$texto\r\n";
+        $conteudo = "$updateId,$nome,$texto,$var \r\n";
         BD_txt_Mysql::BD_txt($conteudo, $updateId);
-        file_put_contents($file, $updateId . ',', FILE_APPEND | LOCK_EX);
+        file_put_contents($file, $updateId . ',', FILE_APPEND | LOCK_EX);//salva os id para nao repetir mensagem no bd
     }
 }
 ?>
